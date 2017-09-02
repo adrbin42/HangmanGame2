0a042e43a27a61b67703acce30d73074d281c4df
Adrienne Bing <adriennebing@me.com>
2017-09-02 12:31:11 -0400

    added on validation and finishing touches

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.get('/', function(req, res) {
   req.session.ltrGuesses = [];
   req.session.letter = '';
   req.session.mysteryWord = undefined;
+  req.session.winner = '';
 
   if(req.session.mysteryWord == undefined){
     let index = Math.floor(Math.random()* words.length);
@@ -50,83 +51,92 @@ app.get('/', function(req, res) {
 });
 
 app.post('/makeGuess',function(req,res) {
-let error_messages = [];
-let errors = "";
 
-req.checkBody("ltr_guess", "Enter a letter silly!").notEmpty();
-req.checkBody("ltr_guess", "I said enter a letter not a number!").isAlpha(); // check if its an alphabet
-req.checkBody("ltr_guess", "You can only enter one letter at a time!").isLength({max: 1});
+  let error_messages = [];
+  let errors = "";
 
-errors = req.validationErrors();
-if (errors) {
-    errors.forEach(function(error) {
-    error_messages.push(error.msg);
-    });
-    res.render("index", {errors: error_messages,
-                         guessesLeft: req.session.maxGuesses});
-    }
+  req.checkBody("ltr_guess", "Enter a letter silly, cannot be blank!").notEmpty();
+  req.checkBody("ltr_guess", "I said enter a letter not a number!").isAlpha(); // check if its an alphabet
+  req.checkBody("ltr_guess", "You can only enter one letter at a time!").isLength({max: 1});
 
-    let letter = req.body.ltr_guess.toUpperCase(),
-        alrdyGuessed,
-        wrongGuess;
-
-    if(req.session.maxGuesses <= 8 && req.session.maxGuesses > 0){
-      for(let i = 0;i<req.session.mysteryWord.length;i++){
-        if(req.session.mysteryWord[i] == letter){
-          req.session.hiddenword[i] = letter;
-        }
-      }
-      if(req.session.mysteryWord.indexOf(letter) != -1){
-      wrongGuess = false;
-        }else{
-      wrongGuess = true;
-      }
-    }
-    req.session.maxGuesses--;
-    if(req.session.ltrGuesses.indexOf(letter+' ') == -1){
-      req.session.ltrGuesses.push(letter + ' ');
-      alrdyGuessed = false;
-    }else{
-      alrdyGuessed = true;
+  errors = req.validationErrors();
+  if (errors) {
+      errors.forEach(function(error) {
+      error_messages.push(error.msg);
+      });
       req.session.maxGuesses++;
     }
-    let tooManyGuesses;
-    if(req.session.maxGuesses < 0){
-      tooManyGuesses = "Duh, too many guesses, gameover dummy, you lose, hit restart!";
-    }else if(req.session.maxGuesses == 0){
+
+      let letter = req.body.ltr_guess.toUpperCase(),
+          alrdyGuessed,
+          makeAFinalGuess,
+          tooManyGuesses,
+          wrongGuess;
+
+      if(req.session.maxGuesses <= 8 && req.session.maxGuesses > 1){
+          for(let i = 0;i<req.session.mysteryWord.length;i++){
+            if(req.session.mysteryWord[i] == letter){
+              req.session.hiddenword[i] = letter;
+            }
+          }
+          if(req.session.mysteryWord.indexOf(letter) != -1){
+          wrongGuess = false;
+            }else{
+          wrongGuess = true;
+          }
+
+      if(req.session.ltrGuesses.indexOf(letter+' ') == -1){
+        req.session.ltrGuesses.push(letter + ' ');
+        alrdyGuessed = false;
+      }else{
+        alrdyGuessed = true;
+        req.session.maxGuesses++;
+      }
+      req.session.maxGuesses--;
+      if(req.session.hiddenword.join('') === req.session.mysteryWord){
+        req.session.winner = true;
+        res.render("winlose",{chkIfWin:req.session.winner});
+      }else{
+        res.render("index", {
+          word:req.session.mysteryWord,
+          hiddenwordOutput:req.session.hiddenword,
+          guessesLeft:req.session.maxGuesses,
+          lettersGuessed:req.session.ltrGuesses,
+          alrdyGuessedError:alrdyGuessed,
+          wrong:wrongGuess,
+          errors: error_messages});
+      }
+    } else if (req.session.maxGuesses == 1){
+      req.session.maxGuesses--;
       tooManyGuesses = "If you finally got it, hit Final Guess button to enter the word!";
+      res.render("index", {
+        word:req.session.mysteryWord,
+        hiddenwordOutput:req.session.hiddenword,
+        guessesLeft:req.session.maxGuesses,
+        lettersGuessed:req.session.ltrGuesses,
+        alrdyGuessedError:alrdyGuessed,
+        wrong:wrongGuess,
+        toomanyguesses:tooManyGuesses,
+        errors: error_messages});
+    }else if(req.session.maxGuesses <= 0) {
+      req.session.maxGuesses--;
+      req.session.winner = false;
+      res.render("winlose",{chkIfWin:req.session.winner});
     }
-    console.log("Already guessed: " + alrdyGuessed);
-    console.log("Wrong guess: " + wrongGuess);
-    console.log("Too many guesses: " + tooManyGuesses);
-    res.render("index", {
-      word:req.session.mysteryWord,
-      hiddenwordOutput:req.session.hiddenword,
-      guessesLeft:req.session.maxGuesses,
-      lettersGuessed:req.session.ltrGuesses,
-      alrdyGuessedError:alrdyGuessed,
-      wrong:wrongGuess,
-      toomanyguesses:tooManyGuesses});
 });
 
 app.post('/finalGuess',function(req,res){
-  console.log(req.body.finalGuess);
-  let finalGuess = req.body.finalGuess.toUpperCase(),
-      winner,
-      winningWord;
-  if(finalGuess === req.session.mysteryWord){
-    winner = true;
-    for(let i = 0;i<finalGuess.length;i++){
-      req.session.hiddenword[i] = finalGuess[i];
+  let finalGuess = req.body.finalGuess.toUpperCase();
+    if(finalGuess === req.session.mysteryWord){
+      req.session.winner = true;
+      for(let i = 0;i<finalGuess.length;i++){
+        req.session.hiddenword[i] = finalGuess[i];
+      }
+      res.render("winlose",{chkIfWin:req.session.winner});
+    }else if(finalGuess !== req.session.mysteryWord) {
+      req.session.winner = false;
+      res.render("winlose",{chkIfWin:req.session.winner});
     }
-  }else {
-    winner = false;
-  }
-    winningWord = req.session.hiddenword.join('');
-    console.log("This is my final guess : " + finalGuess);
-    console.log("This is my mysteryWord: "+req.session.mysteryWord);
-    console.log("Win or lose: "+winner);
-  res.render("winlose",{chkIfWin:winner});
 });
 
 app.post('/restartApp',function(req,res){
